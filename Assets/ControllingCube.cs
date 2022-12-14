c13fa0bf36d6c96b84a0eb284a835c0150b99d11
Daryl Kirchen <kirchendaryl@gmail.com>
2022-12-14 10:23:58 -0600

    Base
    base

---
+++ Assets/ControllingCube.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class ControllingCube : MonoBehaviour {
+
+    private void Start()
+    {
+            EasyController.esconsend.Send_data(1, 0);
+            EasyController.esconsend.Send_data(2, 0);
+            EasyController.esconsend.Send_data(3, 0);
+    }
+    void Update () {
+
+        this.transform.Rotate(EasyController.escon.get_state(1, 0.0f, 5.0f), EasyController.escon.get_state(2), EasyController.escon.get_state(3));
+
+        if(EasyController.escon.get_newstate(101)==1)
+        {
+            this.transform.Translate(new Vector3(0.0f,0.0f,-2.0f));
+        }
+        if (EasyController.escon.get_newstate(101)==0)
+        {
+            this.transform.Translate(new Vector3(0.0f, 0.0f, 2.0f));
+        }
+
+    }
+}
